4a1d64b37139a680d9c2a17de666d6e6fdcbc451
João Medeiros <joaoserj@gmail.com>
2019-09-18 11:41:18 -0300

    notify file prototype base

--- src/main.js
+++ src/main.js
@@ -3,10 +3,11 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
-import firebase from "./boot/firebase";
-
+import firebase from "./plugins/firebase";
+import notify from "./plugins/notify";
 
 Vue.use(firebase);
+Vue.use(notify)
 
 
 Vue.config.productionTip = false;
---
+++ src/plugins/notify.js
@@ -0,0 +1,5 @@
+import Vue from "vue";
+
+export default async () => {
+  Vue.prototype.$notify = () => {};
+};
