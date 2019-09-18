f8765d1ca09e0e9d4b487d88f42e84cd225c3fd5
João Medeiros <joaoserj@gmail.com>
2019-09-18 15:17:55 -0300

    update main.js

--- src/main.js
+++ src/main.js
@@ -4,10 +4,8 @@ import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
 import firebase from "./plugins/firebase";
-import notify from "./plugins/notify";
 
 Vue.use(firebase);
-Vue.use(notify)
 
 
 Vue.config.productionTip = false;
