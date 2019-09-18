ba6fda0e38a6674cf6fda6627faa3a7e178dc8e6
João Medeiros <joaoserj@gmail.com>
2019-09-18 18:32:54 -0300

    all verbs CRUD + Listenner

--- src/page/home.vue
+++ src/page/home.vue
@@ -1,15 +1,89 @@
 <template>
-  <div>
-
-  </div>
+  <div></div>
 </template>
 
 <script>
-  export default {
-    
+export default {
+  data() {
+    return {
+      checklist: {
+        title: "",
+        content: "",
+        complete: false,
+        createdAt: null
+      },
+      listChecklistComplete: [],
+      listChecklistNonComplete: []
+    };
+  },
+  methods: {
+    lodingChecklist() {
+      this.dbChecklist.where("complete", "==", true).onSnapshot(doc => {
+        doc.forEach(complete => {
+          this.listChecklistComplete.push(complete.data());
+        });
+      });
+
+      this.dbChecklist.where("complete", "==", false).onSnapshot(doc => {
+        doc.forEach(nonComplete => {
+          this.listChecklistNonComplete.push(nonComplete.data());
+        });
+      });
+    },
+    createChecklist() {
+      this.dbChecklist
+        .add(this.checklist)
+        .then(ref => {
+          const update = {
+            idChecklist: ref.id,
+            createdAt: this.$timestamp
+          };
+          ref
+            .update(update)
+            .then(() => {
+              // ok
+            })
+            .catch(() => {
+              // erro 2
+            });
+        })
+        .catch(() => {
+          // error
+        });
+    },
+    updateChecklist(object) {
+      this.dbChecklist
+        .update(object)
+        .then(() => {
+          // ok
+        })
+        .catch(() => {
+          // error
+        });
+    },
+    deleteChecklist() {
+      this.dbChecklist
+        .delete()
+        .then(() => {
+          // ok
+        })
+        .catch(() => {
+          // error
+        });
+    }
+  },
+  created() {},
+  computed: {
+    dbChecklist() {
+      if (this.user.uid != null) {
+        return this.$db.collection("app");
+      } else {
+        return null;
+      }
+    }
   }
+};
 </script>
 
 <style lang="scss" scoped>
-
 </style>
