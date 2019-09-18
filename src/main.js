86c18aa89aeb3450d994f8606bcbbaa45879b7dd
João Medeiros <joaoserj@gmail.com>
2019-09-18 10:52:26 -0300

    firebase plugin

--- src/main.js
+++ src/main.js
@@ -1,14 +1,19 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import vuetify from './plugins/vuetify';
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import vuetify from "./plugins/vuetify";
+import firebase from "./boot/firebase";
 
-Vue.config.productionTip = false
+
+Vue.use(firebase);
+
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
   vuetify,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
