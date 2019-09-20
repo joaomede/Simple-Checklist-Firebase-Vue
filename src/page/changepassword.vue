e50189752567a0cc93b0ff29037e4af89504cf54
João Medeiros <joaoserj@gmail.com>
2019-09-20 10:43:07 -0300

    update vue data change password

--- src/page/changepassword.vue
+++ src/page/changepassword.vue
@@ -25,15 +25,11 @@
 export default {
   data() {
     return {
-      show1: false,
-      show2: true,
-      show3: false,
-      show4: false,
-      password: "Password",
+      password1: "",
+      password2: "",
       rules: {
         required: value => !!value || "Required.",
-        min: v => v.length >= 8 || "Min 8 characters",
-        emailMatch: () => "The email and password you entered don't match"
+        min: v => v.length >= 8 || "Min 8 characters"
       }
     };
   }
