18b6c78f2c69e1ec89a757995f04e98a0d1bdc66
João Medeiros <joaoserj@gmail.com>
2019-09-20 01:00:09 -0300

    login template and rules input

--- src/page/login.vue
+++ src/page/login.vue
@@ -1,15 +1,50 @@
 <template>
-  <div>
+  <div class="centralDiv">
+    <v-card
+      max-width="344"
+      class="mx-auto my-card"
+      style="justify-content: center; align-items: center"
+    >
+      <h2 class="text-center">Change Your Password</h2>
+      <v-col cols="12" sm="6">
+        <v-text-field
+          v-model="title"
+          :rules="[rules.required, rules.counter]"
+          label="Title"
+          counter
+          maxlength="20"
+        ></v-text-field>
+      </v-col>
 
+      <v-col cols="12" sm="6">
+        <v-text-field
+          v-model="email"
+          :rules="[rules.required, rules.email]"
+          label="E-mail"
+        ></v-text-field>
+      </v-col>
+    </v-card>
   </div>
 </template>
 
 <script>
-  export default {
-    
+export default {
+  data() {
+    return {
+      title: "Preliminary report",
+      email: "",
+      rules: {
+        required: value => !!value || "Required.",
+        counter: value => value.length <= 20 || "Max 20 characters",
+        email: value => {
+          const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+          return pattern.test(value) || "Invalid e-mail.";
+        }
+      }
+    };
   }
+};
 </script>
 
 <style lang="scss" scoped>
-
 </style>
