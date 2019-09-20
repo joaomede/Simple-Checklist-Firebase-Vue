2b1c781ce0ffb8e3248ee898a670583c9c7c1cb4
João Medeiros <joaoserj@gmail.com>
2019-09-20 01:00:28 -0300

    init template and rules change password

--- src/page/changepassword.vue
+++ src/page/changepassword.vue
@@ -1,16 +1,44 @@
 <template>
-  <div>
-    
-
+  <div class="centralDiv">
+    <v-card
+      max-width="344"
+      class="mx-auto my-card"
+      style="justify-content: center; align-items: center"
+    >
+      <h2 class="text-center">Change Your Password</h2>
+      <v-text-field
+        v-model="password"
+        :append-icon="show1 ? 'visibility' : 'visibility_off'"
+        :rules="[rules.required, rules.min]"
+        :type="show1 ? 'text' : 'password'"
+        name="input-10-1"
+        label="Normal with hint text"
+        hint="At least 8 characters"
+        counter
+        @click:append="show1 = !show1"
+      ></v-text-field>
+    </v-card>
   </div>
 </template>
 
 <script>
-  export default {
-    
+export default {
+  data() {
+    return {
+      show1: false,
+      show2: true,
+      show3: false,
+      show4: false,
+      password: "Password",
+      rules: {
+        required: value => !!value || "Required.",
+        min: v => v.length >= 8 || "Min 8 characters",
+        emailMatch: () => "The email and password you entered don't match"
+      }
+    };
   }
+};
 </script>
 
 <style lang="scss" scoped>
-
 </style>
