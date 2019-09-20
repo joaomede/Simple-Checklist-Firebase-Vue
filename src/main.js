3ef50492f9a5088609af26b2c95fbcfa0f034c1e
João Medeiros <joaoserj@gmail.com>
2019-09-20 00:59:15 -0300

    style.css boot

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
 import firebase from "./plugins/firebase";
+import "./style/style.css"
 
 Vue.use(firebase);
 
