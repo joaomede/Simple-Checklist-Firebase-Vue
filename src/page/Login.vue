e4e7a1ddb97e82918aa872782514fd6c38533d4c
João Medeiros <joaoserj@gmail.com>
2019-09-21 16:30:56 -0300

    Updated Login.vue Rules and Data()

--- src/page/Login.vue
+++ src/page/Login.vue
@@ -36,15 +36,17 @@ export default {
       email: "",
         password: ""
       },
-      email: "",
       rules: {
+        min: v => v.length >= 8 || "Min 8 characters",
         required: value => !!value || "Required.",
-        counter: value => value.length <= 20 || "Max 20 characters",
         email: value => {
           const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
           return pattern.test(value) || "Invalid e-mail.";
         }
-      }
+      },
+
+      dialogRegister: false,
+      dialogForgot: false
     };
   },
   methods: {
